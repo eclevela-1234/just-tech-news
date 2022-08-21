7b739fcb08f13f6f75d3d75719b592b75e400a59
Eliot Cleveland <eclevela@richmond.edu>
2022-08-21 01:05:12 -0400

    routes set

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+//create our User model
+
+class User extends Model {}
+
+User.init(
+  {
+    // Table column definitions go here
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            // this means the pass must be 4 characters long
+            len: [4]
+        }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = User;
