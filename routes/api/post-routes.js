093212a0591a84bf1123550f03b7ea1d7007f036
Eliot Cleveland <eclevela@richmond.edu>
2022-08-22 22:53:15 -0400

    many to many

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-const { User, Post } = require("../../models");
+const { User, Post, Vote } = require("../../models");
+const sequelize = require("../../config/connection");
 
 // get all posts
 router.get("/", (req, res) => {
@@ -7,7 +8,7 @@ router.get("/", (req, res) => {
   Post.findAll({
     //Query configuration
     attributes: ["id", "post_url", "title", "created_at"],
-    order: [['created_at', 'DESC']],
+    order: [["created_at", "DESC"]],
     include: [
       {
         model: User,
@@ -66,8 +67,42 @@ router.post("/", (req, res) => {
     });
 });
 
-//update a posts title
+// put /api/posts/upvote
 
+router.put("/upvote", (req, res) => {
+  // create the vote
+  Vote.create({
+    user_id: req.body.user_id,
+    post_id: req.body.post_id,
+  })
+    .then(() => {
+      // then find the post we just voted on
+      return Post.findOne({
+        where: {
+          id: req.body.post_id,
+        },
+        attributes: [
+          "id",
+          "post_url",
+          "title",
+          "created_at",
+          // use raw MySQL aggregate function query to get a count of how many votes the post has and return it under the name `vote_count`
+          [
+            sequelize.literal(
+              "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+            ),
+            "vote_count",
+          ],
+        ],
+      });
+    })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+//update a posts title
 router.put("/:id", (req, res) => {
   Post.update(
     { title: req.body.title },
@@ -91,21 +126,21 @@ router.put("/:id", (req, res) => {
 });
 
 router.delete("/:id", (req, res) => {
-    Post.destroy({
-      where: {
-        id: req.params.id,
-      },
+  Post.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No user post with this id" });
+        return;
+      }
+      res.json(dbPostData);
     })
-      .then((dbPostData) => {
-        if (!dbPostData) {
-          res.status(404).json({ message: "No user post with this id" });
-          return;
-        }
-        res.json(dbPostData);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 module.exports = router;
---
+++ scratch.js
@@ -0,0 +1,28 @@
+[
+	{
+		"id": 3,
+		"post_url": "https://runbuddy.com/press",
+		"title": "Runbuddy reaches 1 million subscribers",
+		"created_at": "2022-08-22T02:58:44.000Z",
+		"user": {
+			"username": "TopRamen"
+		}
+	},
+	{
+		"id": 1,
+		"post_url": "https://taskmaster.com/press",
+		"title": "Taskmaster goes public!",
+		"created_at": "2022-08-21T22:22:31.000Z",
+		"user": {
+			"username": "TopRamen"
+		}
+	}
+];
+
+[
+	{
+		"id": 1,
+		"username": "TopRamen",
+		"email": "eclevela@richmond.edu"
+	}
+]
