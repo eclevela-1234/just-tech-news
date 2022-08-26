c37de6d05e80c5ed3f04e6c269426402b865bb16
Eliot Cleveland <eclevela@richmond.edu>
2022-08-26 00:12:49 -0400

    login session fuctional, need to add destroy logic

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,7 @@ const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 
 router.get("/", (req, res) => {
+    console.log(req.session);
   Post.findAll({
     //Query configuration
     attributes: [
@@ -44,6 +45,10 @@ router.get("/", (req, res) => {
 });
 
 router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
     res.render('login')
 });
 
