efa19b2f8d84d5969b9807fdf6c5002397686ed5
Eliot Cleveland <eclevela@richmond.edu>
2022-08-27 22:09:17 -0400

    upvote functions

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -107,13 +107,17 @@ router.post("/", (req, res) => {
 // put /api/posts/upvote
 
 router.put("/upvote", (req, res) => {
-  // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote })
-    .then((updatedPostData) => res.json(updatedPostData))
+  // make sure the session exists first
+  console.log(req.session.user_id)
+  if (req.session) {
+    // pass session id along ith all destructured properties on req.body
+      Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+    .then((updatedVoteData) => res.json(updatedVoteData))
     .catch((err) => {
       console.log(err);
       res.status(400).json(err);
     });
+  }
 });
 
 //update a posts title
